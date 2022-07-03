51b95792fb828a3aa6e89dc461a0e444c719b0d9
pshabanov <pshabanov@nurtelecom.kg>
2022-07-03 19:05:55 +0600

    react(ts) + laravel 8

---
+++ resources/js/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+                React component here!
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+const mix = require('laravel-mix');
+
+mix.ts("resources/js/index.tsx", "public/js").react();
